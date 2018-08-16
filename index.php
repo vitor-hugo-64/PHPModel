350e016a39a5e88173b58e42e9bffed24a45579f
Vitor Hugo <vitorhugooliveira64@gmail.com>
2018-08-16 17:21:43 -0300

    Adição das classes de template

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use \Slim\App;
+use \PHPModel\Page;
+
+$config = array( 'settings' => array( 'addContentLengthHeader' => false), 'debug' => true);
+
+$app = new App($config);
+
+$app->get( '/', function ()
+{
+	$page = new Page();
+	$page->drawPage( 'index');
+});
+
+$app->run();
